a20cf37faf5f0eb040290d6eadaa6d4077007695
adri <adrielestevez.78@gmail.com>
2023-09-01 14:59:05 -0300

    te amo

--- Downloads/GIt/hola.py
+++ Downloads/GIt/hola.py
@@ -1,7 +1,5 @@
 import cv2 
-
-face_cascade = cv2.CascadeClassifier("haarcascade_smile.xml")
-
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 cap = cv2.VideoCapture(1)
 
 while True:
